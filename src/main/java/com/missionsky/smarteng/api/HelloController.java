0b24d08d178b944597c1aa456ea48afba0528954
starli-sl <star.li@missionsky.com>
2017-07-19 17:50:06 +0800

    Integrate SLF4J

--- src/main/java/com/missionsky/smarteng/api/HelloController.java
+++ src/main/java/com/missionsky/smarteng/api/HelloController.java
@@ -4,14 +4,18 @@ import com.missionsky.smarteng.model.User;
 import com.missionsky.smarteng.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class HelloController {
     @Autowired
     HelloService helloService;
 
+    private final static Logger logger = LoggerFactory.getLogger(HelloController.class);
     @RequestMapping("/")
     public String index() {
+        logger.info("test log");
         return helloService.getGreeting();
     }
 
