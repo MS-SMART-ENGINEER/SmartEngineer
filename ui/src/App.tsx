a5812e36cb056ba97e5b807e80cfc3a089864eec
MSSKY\john.huang <john.huang@missionsky.com>
2017-07-27 17:20:31 +0800

    material-ui helloworld

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,22 +1,17 @@
 import * as React from 'react';
-import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// import RaisedButton from 'material-ui/RaisedButton';
+import MyComponent from './MyComponent' 
 
-const logo = require('./logo.svg');
 
-class App extends React.Component<{}, {}> {
+class App extends React.Component<any, any> {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
+      <MuiThemeProvider>
+        <MyComponent />
+      </MuiThemeProvider>
+    )
   }
 }
-
+ 
 export default App;
---
+++ ui/src/MyComponent.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+
+class MyComponent extends React.Component<any, any> {
+  render() {
+    return (
+        <RaisedButton label="Hello World" />
+    )
+  }
+}
+ 
+export default MyComponent;
